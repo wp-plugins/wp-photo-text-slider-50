2487be77cfdbd651b803732642ded647ec7d5eaa
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-18 13:55:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-text-slider-50/tags/1.0@439680 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,37 @@
+<!--
+##########################################################################################################################
+###### Project   : Wp photo text slider 50  																		######
+###### File Name : help.php                   																		######
+###### Purpose   : user help file.  																				######
+###### Created   : 30-june-2011                  																	######
+###### Modified  : 30-june-2011                  																	######
+###### Author    : Gopi.R (http://www.gopiplus.com/work/)                       									######
+###### Link      : http://www.gopiplus.com/work/2011/06/02/wordpress-plugin-wp-photo-slider-50/   					######
+##########################################################################################################################
+-->
+
+
+<div class="wrap">
+  <h2>Plugin Management</h2>
+  Click "Go to - Image Management" button to add/update/delete the images. <br />
+  Click "Go to - Gallery Setting" button to update gallery setting.
+  <h2>Plugin Configuration</h2>
+  <ul>
+  <li>1. Drag and Drop the Widget</li>
+  <li>2. Add directly in the theme</li>
+  <li>3. Short code for pages and posts</li>
+  </ul>
+  <a target="_blank" href='http://www.gopiplus.com/work/2011/06/02/wordpress-plugin-wp-photo-slider-50/'>Click here for more help and video tutorial</a>
+  <h2>About Plugin</h2>
+  Plug-in created by <a target="_blank" href='http://www.gopiplus.com/work/'>Gopi</a>.<br />
+  <a target="_blank" href='http://www.gopiplus.com/work/2011/06/02/wordpress-plugin-wp-photo-slider-50/'>Click here</a> to see more information, live demo and video tutorial.<br />
+  <a target="_blank" href='http://www.gopiplus.com/work/plugin-list/'>Click here</a> to download my other plugins.<br />
+  <h2>Useful slideshow plugins</h2>
+  <ul>
+    <li><a target="_blank" href="http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/">Superb slideshow gallery</a></li>
+    <li><a target="_blank" href="http://www.gopiplus.com/work/2010/08/07/new-simple-gallery/">New simple gallery</a></li>
+    <li><a target="_blank" href="http://www.gopiplus.com/work/2010/07/18/excellent-transition-gallery/">Excellent transition gallery</a></li>
+    <li><a target="_blank" href="http://www.gopiplus.com/work/2010/07/18/vertical-scroll-image-slideshow-gallery/">Vertical scroll image slideshow gallery</a></li>
+    <li><a target="_blank" href="http://www.gopiplus.com/work/2010/07/18/translucent-image-slideshow-gallery/">Translucent image slideshow gallery</a></li>
+  </ul>
+</div>
