f7a89538e2a97df9f325c9bcc9e00fc89750b1b1
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-04 07:12:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-text-slider-50/trunk@581636 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,35 @@
+<!--
+/**
+ *     Wp photo text slider 50
+ *     Copyright (C) 2011  www.gopiplus.com
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+-->
+
+
+<div class="wrap">
+  <h2>Plugin Management</h2>
+  <ul>
+  <li>Click "Go to - Image Management" button to add/update/delete the images.</li>
+  <li>Click "Go to - Gallery Setting" button to update gallery setting.</li>
+  </ul>
+  <h2>Plugin Configuration</h2>
+  <ul>
+  <li>1. Drag and Drop the Widget</li>
+  <li>2. Add directly in the theme</li>
+  <li>3. Short code for pages and posts</li>
+  </ul>
+  Check official website for more info <a target="_blank" href='http://www.gopiplus.com/work/2011/06/02/wordpress-plugin-wp-photo-slider-50/'>click here</a><br />
+</div>
